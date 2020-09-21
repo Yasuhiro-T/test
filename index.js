ebd7163378986192518125469d4485d55cea0f3e
Yasuhiro-T <71485481+Yasuhiro-T@users.noreply.github.com>
2020-09-21 12:28:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function setbackgroundDaytime() {
+    document.getElementById( "top-page" ).style.backgroundImage = "url(./image/sea_image_daytime.JPG)";
+    document.getElementById( "footer" ).style.backgroundColor = "rgba(0, 225, 255, 0.8)";
+}
+
+function setbackgroundEvening() {
+    document.getElementById( "top-page" ).style.backgroundImage = "url(./image/sea_image_evening.JPG)";
+    document.getElementById( "footer" ).style.backgroundColor = "rgba(255, 236, 125, 0.8)";
+}
+
+function setbackgroundEvening02() {
+    document.getElementById( "top-page" ).style.backgroundImage = "url(./image/sea_image_evening_02.JPG)";
+    document.getElementById( "footer" ).style.backgroundColor = "rgba(252, 180, 47, 0.8)";
+}
+
+function setbackgroundMorning() {
+    document.getElementById( "top-page" ).style.backgroundImage = "url(./image/sea_image_morning.JPG)";
+    document.getElementById( "footer" ).style.backgroundColor = "rgba(179, 179, 179, 0.8)";
+}
+
+function setbackgroundNight() {
+    document.getElementById( "top-page" ).style.backgroundImage = "url(./image/sea_image_night.JPG)";
+    document.getElementById( "footer" ).style.backgroundColor = "rgba(255, 224, 47, 0.8)";
+}
